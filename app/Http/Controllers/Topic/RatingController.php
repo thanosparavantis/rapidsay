c88e79f703afae03ab882c0faf463d684029bbc8
Thanos Paravantis <p16112@students.cs.unipi.gr>
2017-02-24 14:52:10 +0200

    Refactor rating controller, remove unused imports.

--- app/Http/Controllers/Topic/RatingController.php
+++ app/Http/Controllers/Topic/RatingController.php
@@ -3,48 +3,72 @@
 namespace Forum\Http\Controllers\Topic;
 
 use Auth;
+use Forum\ModelHelper;
+use Forum\Rating;
 use Forum\Events\Topic\RatingCreated;
 use Forum\Events\Topic\RatingDeleted;
-use Forum\Post;
-use Forum\Comment;
-use Forum\Reply;
-use Forum\Rating;
-use Forum\ModelHelper;
 use Forum\Http\Controllers\Controller;
 use Forum\Http\Requests\Topic\RateRequest;
 
+/**
+ * Handles all content ratings.
+ */
 class RatingController extends TopicController
 {
+    /**
+     * Setup authentication middleware.
+     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
+    /**
+     * Called when a post, comment or reply is rated.
+     */
     public function rate(RateRequest $request, $type, $id)
     {
+        // Get the model.
         $item = ModelHelper::resolve($type, $id);
+
+        // Get authenticated user.
         $user = auth()->user();
+
+        //Get rating model.
         $rating = $user->ratings()->where('rateable_type', get_class($item))->where('rateable_id', $id)->first();
 
+        // If rating exists, remove it.
         if ($rating)
         {
+            // Fire rating deleted event.
             event(new RatingDeleted(auth()->user(), $rating));
+
+            // Delete rating.
             $rating->delete();
         }
+        // If rating doesn't exist, add a new one.
         else
         {
+            // Create a new rating model.
             $rating = new Rating(['user_id' => $user->id]);
 
+            // If the user is rating his own content, mark rating as own.
             if ($item->user_id == $user->id) {
                 $rating->own = true;
             }
 
+            // Save rating on item.
             $item->ratings()->save($rating);
+
+            // Fire rating created event.
             event(new RatingCreated($rating));
         }
 
+        // JSON response.
         return response()->json([
-            'html' => view('topic.partials.rating', ['item' => $item])->render(),
+            'html' => view('topic.partials.rating', [
+                'item' => $item,
+            ])->render(),
         ]);
     }
 }
