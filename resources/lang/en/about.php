2319b1c751ef853d442abaf6e7bc8b448f8d2079
Thanos Paravantis <p16112@students.cs.unipi.gr>
2017-03-26 13:20:59 +0300

    Revert "Merge branch 'home-update'"
    This reverts commit 8de98f2b570186326ff37d9bdd4162f4f7e951ed.

--- resources/lang/en/about.php
+++ resources/lang/en/about.php
@@ -1,17 +1,6 @@
 <?php
 
 return [
-    'points'            => [
-        'difference'    => 'Differenciate yourself',
-        'alternative'   => 'Join an alternative social network',
-        'ratings'       => 'Gain ratings at your posts',
-        'reputation'    => 'Build reputation',
-    ],
-
-    'buttons'           => [
-        'register'      => 'Register with our highly interactive network',
-    ],
-
     'terms-of-use'      => 'Terms of Use',
     'privacy-policy'    => 'Privacy Policy',
     'back'              => 'Back',
